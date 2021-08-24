163d7a254c520ca8ef7f85c89ce602402929f3e0
Yako02 <carlosmezag57@gmail.com>
2021-08-23 18:28:08 -0600

    Mostrar billetes retiros, mejoras UI

--- src/Menu/frmRetiros.java
+++ src/Menu/frmRetiros.java
@@ -127,6 +127,8 @@ public class frmRetiros extends javax.swing.JFrame {
             crea_ubicación.mkdirs();
             Formatter crear = new Formatter(ubi3+archivo);
             
+            
+            
             crear.format("%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s",
                     "Saldo="+(a.get(0).Saldo-nSaldo),           
                     "Q200="+(a.get(0).Q200-B200),       //IMPORTANTE QUE CONCUERDEN EL FORMATO DE Qnumero
@@ -176,7 +178,7 @@ public class frmRetiros extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
         setResizable(false);
-        this.getContentPane().setBackground(java.awt.Color.LIGHT_GRAY);
+        this.getContentPane().setBackground(java.awt.Color.WHITE);
         
         ordenarPorNumero(registros);
         ordenarPorNumero(registros2);
@@ -240,7 +242,6 @@ public class frmRetiros extends javax.swing.JFrame {
                     break;
                 }
             }catch(Exception e){}
-            
         }
         
         
@@ -282,6 +283,24 @@ public class frmRetiros extends javax.swing.JFrame {
         
         return B;
     }
+    
+    private void imprimir(int B200, int B100, int B50, int B20, int B10, int B5, int B1, String saldo){
+        String Q200 = String.valueOf(B200);
+        String Q100 = String.valueOf(B100);
+        String Q50 = String.valueOf(B50);
+        String Q20 = String.valueOf(B20);
+        String Q10 = String.valueOf(B10);
+        String Q5 = String.valueOf(B5);
+        String Q1 = String.valueOf(B1);
+        txtretirofinal.setText(saldo);
+        txt200.setText(Q200);
+        txt100.setText(Q100);
+        txt50.setText(Q50);
+        txt20.setText(Q20);
+        txt10.setText(Q10);
+        txt5.setText(Q5);
+        txt1.setText(Q1);
+    }
 
 
     @SuppressWarnings("unchecked")
@@ -295,6 +314,24 @@ public class frmRetiros extends javax.swing.JFrame {
         btnAceptar = new javax.swing.JButton();
         btnAceptar1 = new javax.swing.JButton();
         txtretiro = new javax.swing.JSpinner();
+        jPanel2 = new javax.swing.JPanel();
+        tarjeta1 = new javax.swing.JLabel();
+        txtretirofinal = new javax.swing.JTextField();
+        tarjeta2 = new javax.swing.JLabel();
+        txt200 = new javax.swing.JTextField();
+        tarjeta3 = new javax.swing.JLabel();
+        tarjeta4 = new javax.swing.JLabel();
+        tarjeta5 = new javax.swing.JLabel();
+        tarjeta6 = new javax.swing.JLabel();
+        tarjeta7 = new javax.swing.JLabel();
+        tarjeta8 = new javax.swing.JLabel();
+        txt20 = new javax.swing.JTextField();
+        txt100 = new javax.swing.JTextField();
+        txt50 = new javax.swing.JTextField();
+        txt10 = new javax.swing.JTextField();
+        txt5 = new javax.swing.JTextField();
+        txt1 = new javax.swing.JTextField();
+        tarjeta9 = new javax.swing.JLabel();
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
@@ -357,37 +394,188 @@ public class frmRetiros extends javax.swing.JFrame {
 
         txtretiro.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
 
+        jPanel2.setBackground(new java.awt.Color(51, 51, 51));
+        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        tarjeta1.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta1.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta1.setText("Retiro:");
+
+        txtretirofinal.setEditable(false);
+        txtretirofinal.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        tarjeta2.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta2.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta2.setText("Q200:");
+
+        txt200.setEditable(false);
+        txt200.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        tarjeta3.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta3.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta3.setText("Q10:");
+
+        tarjeta4.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta4.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta4.setText("Q100:");
+
+        tarjeta5.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta5.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta5.setText("Q50:");
+
+        tarjeta6.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta6.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta6.setText("Q20:");
+
+        tarjeta7.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta7.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta7.setText("Q5:");
+
+        tarjeta8.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta8.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta8.setText("Q1:");
+
+        txt20.setEditable(false);
+        txt20.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        txt100.setEditable(false);
+        txt100.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        txt50.setEditable(false);
+        txt50.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        txt10.setEditable(false);
+        txt10.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        txt5.setEditable(false);
+        txt5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        txt1.setEditable(false);
+        txt1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+
+        tarjeta9.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
+        tarjeta9.setForeground(new java.awt.Color(255, 255, 255));
+        tarjeta9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        tarjeta9.setText("Retiro en Billetes");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(tarjeta3)
+                    .addComponent(tarjeta2)
+                    .addComponent(tarjeta4)
+                    .addComponent(tarjeta5)
+                    .addComponent(tarjeta6)
+                    .addComponent(tarjeta7)
+                    .addComponent(tarjeta8))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txt200)
+                    .addComponent(txt100, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txt20)
+                    .addComponent(txt50)
+                    .addComponent(txt10, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txt5)
+                    .addComponent(txt1))
+                .addContainerGap())
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(tarjeta1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtretirofinal, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(8, 8, 8))
+            .addComponent(tarjeta9, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(tarjeta9, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta1)
+                    .addComponent(txtretirofinal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt200))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt100))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt50, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt20, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txt5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tarjeta7, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tarjeta8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btnAceptar1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(txtretiro, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING))
-                .addGap(53, 53, 53))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(66, 66, 66)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txtretiro, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING))
+                        .addGap(74, 74, 74))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnAceptar1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)))
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(26, 26, 26))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(txtretiro, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnAceptar)
-                    .addComponent(btnAceptar1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtretiro, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(100, 100, 100)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnAceptar)
+                            .addComponent(btnAceptar1)))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(24, 24, 24))
         );
 
@@ -420,6 +608,7 @@ public class frmRetiros extends javax.swing.JFrame {
                 E = 3;
             }else{
                 int saldo = Integer.parseInt(txtretiro.getValue().toString());
+                String retiro = txtretiro.getValue().toString();
                 B200 = getBilletes(a.get(0).Q200, 200, saldo);
                 B100 = getBilletes(a.get(0).Q100, 100, saldo-B200*200);
                 B50 = getBilletes(a.get(0).Q50, 50, saldo-B200*200-B100*100);
@@ -428,6 +617,7 @@ public class frmRetiros extends javax.swing.JFrame {
                 B5 = getBilletes(a.get(0).Q5, 5, saldo-B200*200-B100*100-B50*50-B20*20-B10*10);
                 B1 = getBilletes(a.get(0).Q1, 1, saldo-B200*200-B100*100-B50*50-B20*20-B10*10-B5*5);
                 modCajero(B200, B100, B50, B20, B10, B5, B1, saldo);
+                imprimir(B200, B100, B50, B20, B10, B5, B1, retiro);
 
                 d.get(0).Limite = d.get(0).Limite - saldo;
                 a.get(0).Saldo = a.get(0).Saldo - saldo;
@@ -497,7 +687,25 @@ public class frmRetiros extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel tarjeta;
+    private javax.swing.JLabel tarjeta1;
+    private javax.swing.JLabel tarjeta2;
+    private javax.swing.JLabel tarjeta3;
+    private javax.swing.JLabel tarjeta4;
+    private javax.swing.JLabel tarjeta5;
+    private javax.swing.JLabel tarjeta6;
+    private javax.swing.JLabel tarjeta7;
+    private javax.swing.JLabel tarjeta8;
+    private javax.swing.JLabel tarjeta9;
+    private javax.swing.JTextField txt1;
+    private javax.swing.JTextField txt10;
+    private javax.swing.JTextField txt100;
+    private javax.swing.JTextField txt20;
+    private javax.swing.JTextField txt200;
+    private javax.swing.JTextField txt5;
+    private javax.swing.JTextField txt50;
     private javax.swing.JSpinner txtretiro;
+    private javax.swing.JTextField txtretirofinal;
     // End of variables declaration//GEN-END:variables
 }
